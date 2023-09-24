28f8b8ed89bf6fc32b556f89105fd0a10ee40170
karamithat <mithatkara93@gmail.com>
2023-09-24 23:22:14 +0300

    initialize project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "/logo.png";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <section className="h-wrapper">
+        <div className="flexCenter paddings innerWidth h-container">
+          <img src={logo} alt="logo" width={100} />
+          <div className="h-menu">
+            <a href="">Residencies</a>
+            <a href="">Our Value</a>
+            <a href="">Contact Us</a>
+            <a href="">Get Started</a>
+            <button className="button">
+              <a href="">Contact</a>
+            </button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Header;
