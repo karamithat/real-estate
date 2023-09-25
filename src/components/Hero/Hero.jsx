7a831875f77ec923599368362d9db33e3a4977df
karamithat <mithatkara93@gmail.com>
2023-09-25 23:20:54 +0300

    header and hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import './Hero.css'
+import heroImage from "/hero-image.png"
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='hero-wrapper'>
+      <div className="paddings innerWidth hero-container flexCenter">
+        <div className="hero-left">
+           
+        </div>
+        <div className="flexCenter hero-right">
+            <div className="image-container">
+              <img src={heroImage} alt="" />
+            </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
