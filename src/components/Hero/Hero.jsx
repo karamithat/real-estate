ba563c5eca25ed51fa1f98bc7628f74882998ca2
karamithat <mithatkara93@gmail.com>
2023-09-26 23:02:30 +0300

    hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,18 @@ const Hero = () => {
     <section className='hero-wrapper'>
       <div className="paddings innerWidth hero-container flexCenter">
         <div className="hero-left">
-           
+           <div className="hero-title">
+            <h1>
+              Discover <br />Most Suitable <br />Property
+            </h1>
+           </div>
+           <div className="hero-des">
+            <span>Find a variety of properties that suit you very easilty</span>
+            <span>Forget all difficulties in finding a residence for you</span>
+           </div>
+           <div className="search-bar">
+            search bar
+           </div>
         </div>
         <div className="flexCenter hero-right">
             <div className="image-container">
